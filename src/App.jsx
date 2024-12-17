03eb52b82b323de84cb8c3e19c941c1f4317ac98
Joshua Zega <hedywidja321@gmail.com>
2024-12-18 04:24:02 +0700

    Rolling back

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from "./components/pages/HomePage/HomePage";
 import Login from "./components/pages/Login/Login";
 import Register from "./components/pages/Register/Register";
+
+
 
 const App = () => (
   <HelmetProvider>
