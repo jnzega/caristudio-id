3f121441083f0819d2ff3deb16426fbcaf9ecd96
Joshua Zega <hedywidja321@gmail.com>
2024-12-18 04:41:30 +0700

    Adding some width device rules

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,28 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from "./components/pages/HomePage/HomePage";
 import Login from "./components/pages/Login/Login";
 import Register from "./components/pages/Register/Register";
+import useWindowWidth from './components/hook/useWindowWidth/useWindowWidth';
 
+const App = () => {
+  const width = useWindowWidth();
+  const isMobileView = width < 1024;
 
+  return (
 
-const App = () => (
-  <HelmetProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  </HelmetProvider>
-);
-
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {isMobileView ? (
+            <p>Tolong buka di tampilan desktop atau buka di situs ini di komputer Anda</p>
+          ) : (
+            <p>Tampilan normal untuk desktop (≥ 1024px)</p>
+          )}
+        </Routes>
+      </Router>
+    </HelmetProvider>
+  );
+};
 export default App;
