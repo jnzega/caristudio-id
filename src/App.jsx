9b63fdd0a68694b3646008c5bbfbab1091139ed7
Joshua Zega <hedywidja321@gmail.com>
2024-12-18 04:17:11 +0700

    Changing to Hashroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from "./components/pages/HomePage/HomePage";
 import Login from "./components/pages/Login/Login";
@@ -7,7 +7,7 @@ import Register from "./components/pages/Register/Register";
 
 const App = () => (
   <HelmetProvider>
-    <Router basename="/caristudio-id">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
