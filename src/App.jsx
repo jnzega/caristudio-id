5977ebf9f882614f75fe692c907e09396e5ed221
Joshua Zega <hedywidja321@gmail.com>
2024-12-21 15:22:19 +0700

    Changing the BrowserRouter to HashRouter for Github Pages Compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import HomePage from "./components/pages/HomePage/HomePage";
 import Login from "./components/pages/Login/Login";
